fb72451319fe5fdb43db24a42c06706c032f1279
ThuyN <thuynguyen.nht89@yahoo.com>
2020-04-29 09:32:33 -0700

    working app

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0,
 	PRIMARY KEY (id)
 );
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+$(function () {
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burger-input").val().trim(),
+            devoured: "0"
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("New Burger added.");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".change-devour").on("click", function (event) {
+
+        var id = $(this).data("id");
+
+        var devourState = {
+            devoured: "1"
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devourState
+        }).then(
+            function () {
+                console.log("changed sleep to", devoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+});

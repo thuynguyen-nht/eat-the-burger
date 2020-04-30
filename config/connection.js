18f10b99b60fe677ec96b05bb352fcc235ede446
ThuyN <thuynguyen.nht89@yahoo.com>
2020-04-29 20:42:33 -0700

    edit connection with missing else

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,15 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
-
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
 connection.connect();
 
 module.exports = connection;
